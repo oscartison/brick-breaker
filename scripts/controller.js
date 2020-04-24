3df6ae83b42fcfc09d1606c39f0a533b3edf4301
55315 <55315@etu.he2b.be>
2020-04-24 16:31:18 +0200

    Étape 1.5: Ajustements du mouvement de la raquette

--- scripts/controller.js
+++ scripts/controller.js
@@ -6,10 +6,10 @@
 $(document).ready(() => {
     const paddle = new Paddle(Scene_Width / 2 - (Paddle_Width / 2));
     displayPaddle(paddle);
-    
-    $(document).mousemove(function(e) {
-     const new_left = e.clientX - $("#gameContainer").offset().left - (Paddle_Width / 2);
+
+    $(document).mousemove(function (e) {
+        const new_left = e.clientX - $("#gameContainer").offset().left - (Paddle_Width / 2);
         paddle.moveTo(new_left);
         displayPaddle(paddle);
-     });
     });
+});
--- scripts/model/paddle.js
+++ scripts/model/paddle.js
@@ -4,8 +4,8 @@ class Paddle {
      * constructs a new paddle at left from the left side of the game
      * @param {number} left 
      */
-    constructor(left){
-        if(left < 0 || left > Scene_Width - Paddle_Width){
+    constructor(left) {
+        if (left < 0 || left > Scene_Width - Paddle_Width) {
             throw new Error("value of left is not possible");
         }
         this._left = left;
@@ -14,7 +14,7 @@ class Paddle {
     /**
      * a getter for how much left the paddle is situated
      */
-    get left(){
+    get left() {
         return this._left;
     }
 
@@ -22,10 +22,13 @@ class Paddle {
      * moves the paddle at position left
      * @param {number} left the new position of the paddle
      */
-    moveTo(left){
-        if(left < 0 || left > Scene_Width - Paddle_Width){
-            throw new Error("value of left is not possible");
+    moveTo(left) {
+        if (left > (Scene_Width - Paddle_Width)) {
+            this._left = Scene_Width - Paddle_Width;
+        } else if (left < 0) {
+            this._left = 0;
+        } else {
+            this._left = left;
         }
-        this._left = left;
     }
 }
