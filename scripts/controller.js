e26046365bbce3e62a911643257e0eec4358612f
55315 <55315@etu.he2b.be>
2020-04-24 16:22:28 +0200

    exercice 1.4 une raquette controlee par la souris

---
+++ scripts/controller.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * when the mouse moves the paddle moves at the same width as the mouse
+ */
+$(document).ready(() => {
+    const paddle = new Paddle(Scene_Width / 2 - (Paddle_Width / 2));
+    displayPaddle(paddle);
+    
+    $(document).mousemove(function(e) {
+     const new_left = e.clientX - $("#gameContainer").offset().left - (Paddle_Width / 2);
+        paddle.moveTo(new_left);
+        displayPaddle(paddle);
+     });
+    });
