b303c305bdf362e964bea0504ece0d31f35191aa
55315 <55315@etu.he2b.be>
2020-05-04 18:02:23 +0200

    Question 3.1 et 3.2 – Une brique dans le code et afficher une brique

--- scripts/constants.js
+++ scripts/constants.js
@@ -5,3 +5,6 @@ const Paddle_Width = 96;
 const Paddle_Height = 16;
 
 const Ball_Radius = 12;
+
+const Bricks_Colums = 10;
+const Bricks_Rows = 3;
---
+++ scripts/model/brick.js
@@ -0,0 +1,27 @@
+class Brick {
+
+    /**
+     * contrcuts an object of the vlass Brick
+     * @param {number} x the horizontal position of the brick 
+     * @param {number} y the vertical position of the brick 
+     */
+    constructor(x, y) {
+        this._x = x;
+        this._y = y;
+    }
+
+    /**
+     * gets the horizontal position of the brick 
+     */
+    get x() {
+        return this._x;
+    }
+
+    /**
+     * gets the vertical position of the brick 
+     */
+    get y() {
+        return this._y;
+    }
+
+}
